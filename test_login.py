68298aa30ff29d7978b34835ee3c1deba369623b
Subaina Monib <subainamonib3@gmail.com>
2026-06-15 22:16:34 +0500

    Clean up codebase: remove stale skeleton, npm junk, test_login.py

--- test_login.py
+++
@@ -1,5 +0,0 @@
-def login(username, password):
-    query = f"SELECT * FROM users WHERE username={username}"
-    user = db.execute(query)
-    if user and user.password == password:
-        return True
